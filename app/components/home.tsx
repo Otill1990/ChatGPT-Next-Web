2b970158f1245a7a747a908b499fa515e844812d
Britton <166590019+Otill1991@users.noreply.github.com>
2025-02-13 06:06:07 -0300

    update.

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -16,6 +16,8 @@ import { ErrorBoundary } from "./error";
 
 import { getISOLang, getLang } from "../locales";
 
+import { useNavigate } from "react-router-dom";
+
 import {
   HashRouter as Router,
   Route,
@@ -174,6 +176,13 @@ function Screen() {
     loadAsyncGoogleFont();
   }, []);
 
+  //jump auth page
+  const accessStore = useAccessStore();
+  const navigate = useNavigate();
+  if (!accessStore.isAuthorized()) {
+    navigate(Path.Auth)
+  }
+  
   if (isArtifact) {
     return (
       <Routes>
